7f424d73f70bdfb6d6dd0ba9499e3ab9506ee038
Sheggy <horse741@ya.ru>
2024-03-11 20:42:16 +0500

    feat: implements experiments controller methods

--- ServerApp/app/Http/Controllers/ExperimentController.php
+++ ServerApp/app/Http/Controllers/ExperimentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Interface\CrudController;
 use App\Http\Resources\ExperimentResource;
 use App\Models\Experiment;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ExperimentController extends Controller implements CrudController
@@ -18,21 +19,38 @@ class ExperimentController extends Controller implements CrudController
 
     public function show($id, Request $request)
     {
-        // TODO: Implement show() method.
+        $data = Experiment::query()->find($id);
+        return new ExperimentResource($data);
     }
 
     public function create(Request $request)
     {
-        // TODO: Implement create() method.
+        $data = Experiment::query()->create([
+            'name' => $request->name,
+            'number' => $request->nubmer,
+            'date' => $request->date,
+            'user_id' => $request->user_id,
+            'tool_id' => $request->tool_Id
+        ]);
+        return new ExperimentResource($data);
     }
 
     public function update($id, Request $request)
     {
-        // TODO: Implement update() method.
+        Experiment::query()->find($id)->update([
+            'name' => $request->name,
+            'number' => $request->nubmer,
+            'date' => $request->date,
+            'user_id' => $request->user_id,
+            'tool_id' => $request->tool_Id
+        ]);
+        $data = Experiment::query()->find($id);
+        return new ExperimentResource($data);
     }
 
     public function delete($id)
     {
-        // TODO: Implement delete() method.
+        User::query()->find($id)->delete();
+        return 0;
     }
 }
