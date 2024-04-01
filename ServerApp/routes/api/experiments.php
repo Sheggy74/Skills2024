12b0987a22ccd56ed0a2c8fb852c009ad97f1b4c
Dreshe4ka <dre478549@gmail.com>
2024-04-01 21:22:28 +0500

    adminauth

--- ServerApp/routes/api/experiments.php
+++ ServerApp/routes/api/experiments.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ExperimentController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('experiments')
-    ->middleware('auth:api')
+    ->middleware('auth:api', 'adminauth')
     ->controller(ExperimentController::class)
     ->group(function (){
         Route::get('','index');
