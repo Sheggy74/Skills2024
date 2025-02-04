b20ddc8c63ba3d7cb83dfc04ed6199401c404395
Rustem <rustemfarshatov>
2025-02-04 17:00:29 +0300

    убрал ошибка

--- ClientApp/src/app/modules/workspace/pages/plan/plan-page.component.ts
+++ ClientApp/src/app/modules/workspace/pages/plan/plan-page.component.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import { Component, inject } from '@angular/core';
 import { WorkspaceService } from '../../services/workspace.service';
 import { StateService } from 'src/app/services/StateService/state.service';
 import { PlanService } from '../../services/plan.service';
 
-=======
-import { Component ,inject} from '@angular/core';
-import { TaskWorkService } from '../../services/task-work.service';
->>>>>>> 537eabb5a2d64bc1545bcdf41bb2eba4ae175d45
 
 @Component({
   selector: 'app-plan-plage',
@@ -15,7 +10,6 @@ import { TaskWorkService } from '../../services/task-work.service';
   styleUrl: './plan-page.component.css'
 })
 export class PlanPageComponent {
-<<<<<<< HEAD
   workspaceService = inject(WorkspaceService);
   planService = inject(PlanService)
   stateService = inject(StateService)
@@ -32,11 +26,5 @@ export class PlanPageComponent {
     // this.planService.getPerformers(Number.parseInt(this.userId ?? ''));
     this.planService.updateUserAndPerformers(Number.parseInt(this.userId ?? '0'))
     this.planService.updateTopics();
-=======
-  taskReportService=inject(TaskWorkService);
-  date:Date=new Date();
-  report(){
-    this.taskReportService.reportTaskDate(this.date)
->>>>>>> 537eabb5a2d64bc1545bcdf41bb2eba4ae175d45
   }
 }
