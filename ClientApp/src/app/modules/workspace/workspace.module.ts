a2c94f297b6a3b839f2c8db7399b4c903cf76970
sheggy74 <horse741@ya.ru>
2025-02-04 22:57:32 +0500

    Порядок планов front

--- ClientApp/src/app/modules/workspace/workspace.module.ts
+++ ClientApp/src/app/modules/workspace/workspace.module.ts
@@ -21,7 +21,7 @@ import { ToolbarModule } from 'primeng/toolbar';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ScrollerModule } from 'primeng/scroller';
 import { TableModule } from 'primeng/table';
-import {AvatarModule} from 'primeng/avatar';
+import { AvatarModule } from 'primeng/avatar';
 import { ProjectComponent } from '../project/pages/project.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
@@ -29,7 +29,6 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { TagModule } from 'primeng/tag';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ProjectModule } from '../project/project.module';
-import { AddProjectComponent } from '../project/pages/add-project/add-project.component';
 import { TabMenuModule } from 'primeng/tabmenu';
 import { TaskSidebarComponent } from './components/task-sidebar/task-sidebar.component';
 import { TabViewModule } from 'primeng/tabview';
@@ -40,6 +39,8 @@ import { PlanPageComponent } from './pages/plan/plan-page.component';
 import { TaskComponent } from './components/task/task.component';
 import { PlanComponent } from './components/plan/plan.component';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { MesComponentsModule } from 'src/app/Shared/MESComponentsModule/mes-components.module';
+import { MesDirectivesModule } from 'src/app/Shared/MESDirectives/mes-directives.module';
 
 
 @NgModule({
@@ -75,7 +76,9 @@ import { InputNumberModule } from 'primeng/inputnumber';
     AvatarGroupModule,
     CalendarModule,
     InputNumberModule,
-    DividerModule
+    DividerModule,
+    MesComponentsModule,
+    MesDirectivesModule
   ]
 
 })
