4f9a5a03b788386a85a28c2431512917fed33451
sheggy74 <horse741@ya.ru>
2025-02-05 08:09:08 +0500

    Презентация

--- ClientApp/src/app/modules/project/services/user-role.service.ts
+++ ClientApp/src/app/modules/project/services/user-role.service.ts
@@ -7,35 +7,35 @@ import { BaseApiService } from 'src/app/services/BaseApiService/base-api.service
 @Injectable({
   providedIn: 'root'
 })
-export class UserRoleService extends BaseApiService{
+export class UserRoleService extends BaseApiService {
 
   localApiPath = this.apiURL + '/project/users';
-  
+
   users = new BehaviorSubject<UserRolePr[]>([]);
-  selectedUsers = new BehaviorSubject<UserRolePr[]|undefined>(undefined);
-  selectedRoleUsers=new BehaviorSubject<UserRolePr[]|undefined>(undefined);
+  selectedUsers = new BehaviorSubject<UserRolePr[] | undefined>(undefined);
+  selectedRoleUsers = new BehaviorSubject<UserRolePr[] | undefined>(undefined);
 
-  isLoading=new BehaviorSubject<boolean>(true);
+  isLoading = new BehaviorSubject<boolean>(true);
 
-  async updateData(){
-    this.users.next(await this.getUserRole());
+  async updateData() {
+    // this.users.next(await this.getUserRole());
   }
 
-  getUserRole():Promise<UserRolePr[]>{
-      let retValue = lastValueFrom(this.http.get<UserRolePr[]>(this.localApiPath)
-        .pipe(
-          map((UserRole:any)=>{
-            console.log(UserRole.data);
-            return UserRole.data;
-          }),
+  getUserRole(): Promise<UserRolePr[]> {
+    let retValue = lastValueFrom(this.http.get<UserRolePr[]>(this.localApiPath)
+      .pipe(
+        map((UserRole: any) => {
+          console.log(UserRole.data);
+          return UserRole.data;
+        }),
         catchError(this.exceptionService.getErrorHandlerList())));
     return retValue;
   }
 
-  getUserRoleID(id:number):Promise<UserRolePr[]>{
-    let retValue = lastValueFrom(this.http.get<UserRolePr[]>(this.apiURL+'/project/user/'+id)
+  getUserRoleID(id: number): Promise<UserRolePr[]> {
+    let retValue = lastValueFrom(this.http.get<UserRolePr[]>(this.apiURL + '/project/user/' + id)
       .pipe(
-        map((UserRole:any)=>{
+        map((UserRole: any) => {
           console.log(UserRole.data);
           this.users.next(UserRole.data);
           return UserRole.data;
